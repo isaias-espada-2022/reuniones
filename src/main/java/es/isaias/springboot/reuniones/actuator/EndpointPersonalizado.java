e5b85a6ebe04654463b024045f9daebdf168238a
isaias-espada-2022 <isaias.espada.2022@gmail.com>
2025-09-10 21:19:42 +0200

    Actuator-personalizado

---
+++ src/main/java/es/isaias/springboot/reuniones/actuator/EndpointPersonalizado.java
@@ -0,0 +1,16 @@
+package es.isaias.springboot.reuniones.actuator;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+@Component
+@Endpoint(id="personalizado")
+public class EndpointPersonalizado {
+
+    @ReadOperation
+    public String getAlgo() {
+        return "algo";
+    }
+
+}
