1fea90ef5ae75d4450338df7d9add2fe6372b750
isaias-espada-2022 <isaias.espada.2022@gmail.com>
2025-09-10 20:49:42 +0200

    Servicios Rest

--- src/main/java/es/isaias/springboot/reuniones/data/PersonaRepository.java
+++ src/main/java/es/isaias/springboot/reuniones/data/PersonaRepository.java
@@ -2,6 +2,8 @@ package es.isaias.springboot.reuniones.data;
 
 import es.isaias.springboot.reuniones.models.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="personas")
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
 }
--- src/main/java/es/isaias/springboot/reuniones/data/ReunionRepository.java
+++ src/main/java/es/isaias/springboot/reuniones/data/ReunionRepository.java
@@ -2,6 +2,8 @@ package es.isaias.springboot.reuniones.data;
 
 import es.isaias.springboot.reuniones.models.Reunion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="reuniones")
 public interface ReunionRepository extends JpaRepository<Reunion, Long> {
 }
